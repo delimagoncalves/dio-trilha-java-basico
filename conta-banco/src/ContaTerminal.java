eddd54bde440b26e6b64360985842da2381ec677
Gilson de lima <>
2024-08-24 13:40:40 -0300

    Arquivo ContaTerminal.java criado

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,7 @@
+import br.com.diodesafio.javabasico.model.ContaTerminal;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ContaTerminal conta = new ContaTerminal();
+    }
+}
