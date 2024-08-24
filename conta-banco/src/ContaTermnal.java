d8b447e969e41e254f39d6f4a3ca9e61e47338bf
Gilson de lima <>
2024-08-24 14:48:22 -0300

    Alteracao do arquivo App.java para ContaTerminal.java

--- conta-banco/src/ContaTermnal.java
+++ conta-banco/src/ContaTermnal.java
@@ -1,15 +1,15 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class App {
+public class ContaTermnal {
     public static void main(String[] args) throws Exception {
 
         double saldo = 0;
-
+        //Utiliza a classe Scanner para obter a entrada do usuário
         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Por favor, digite seu nome: ");
-        String nomeCliente = scan.nextLine();
+        String nomeCliente = scan.nextLine(); 
 
         System.out.println("Por favor, digite o número da agência: ");
         int numeroAgencia = scan.nextInt();
@@ -24,6 +24,5 @@ public class App {
         System.out.printf("\"Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %s e seu saldo R$ %.2f já está disponível para saque.\"",nomeCliente, numeroAgencia,numeroConta,saldo);
 
         scan.close();
-    
     }
 }
